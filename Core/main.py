43684a35797e847242e8ba5cf4c0ea6c67b40f96
EBOHOC <pudelko123>
2021-02-28 21:53:35 +0100

    subplot

---
+++ Core/main.py
@@ -0,0 +1,28 @@
+import os
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+from scipy import signal
+
+
+path = r'C:/Users/EBOHOC/Documents/SoundAnalysis/SoundAnalysis/Sample_Data'
+
+lista = list(os.listdir(path))
+lista_enum = list(enumerate(lista))
+print(lista_enum)
+
+for item in lista_enum:
+    print(str(item[0]) + ' - ' + item[1])
+
+selection = input("Select file for analysis: ")
+name = lista_enum[int(selection)]
+sample_rate, data = wavfile.read(os.path.join(path, name[1]))
+f, t, Sxx = signal.spectrogram(data, sample_rate)
+plt.subplot(1,2,1)
+plt.plot(data)
+plt.subplot(1,2,2)
+plt.pcolormesh(t, f, Sxx, shading='gouraud')
+plt.show()
+
+
+
+
